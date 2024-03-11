6dcdea14534ed9481819711e76f31eb1f5d7c22d
Lena Lurye <frau.lurye@gmail.com>
2024-03-11 20:29:12 +0100

    greet the user

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,5 @@
 #!/usr/bin/env node
+import askUser from "../src/cli.js";
 console.log("Welcome to the Brain Games!");
+let name = askUser("May I have your name? ");
+console.log("Hello, " + name + "!");
---
+++ src/cli.js
@@ -0,0 +1,6 @@
+import readlineSync from "readline-sync";
+
+export default (prompt) => {
+  const answer = readlineSync.question(prompt);
+  return answer;
+};
